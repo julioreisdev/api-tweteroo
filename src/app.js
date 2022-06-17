793fbeb3919cfcdf5efe9b39519227183e9e50a9
julioreisdev <juliosteam0352@gmail.com>
2022-06-17 15:47:41 -0300

    Adicionando estrutura do projeto

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+
+const server = express();
+server.use(cors());
+
+server.get("/", (request, response) => {
+  response.send("OK");
+});
+
+server.listen(5000);
